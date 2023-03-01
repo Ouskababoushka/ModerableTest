823506bf305939e9aa330ede8258223494285759
Valentin Chauveau <valentin.korea@gmail.com>
2023-03-01 21:06:51 +0100

    fetch response and update instance accordingly before saving

--- app/models/concerns/moderable.rb
+++ app/models/concerns/moderable.rb
@@ -1,5 +1,31 @@
+require 'httparty'
+
 module Moderable
   extend ActiveSupport::Concern
 
-  # Votre code
+  class_methods do
+    def moderated_columns
+      @moderated_columns || []
+    end
+
+    def moderated_columns=(columns)
+      @moderated_columns = columns
+    end
+  end
+
+  def moderate_content
+    self.class.moderated_columns.each do |column|
+      content = send(column)
+      next if content.blank?
+
+      response = HTTParty.get("https://moderation.logora.fr/predict?text=#{content}")
+      # p JSON.parse(response.body)
+      if response.code == 200
+        prediction = JSON.parse(response.body)["prediction"]["0"]
+        update(is_accepted: prediction > 0.5)
+      else
+        errors.add(column, "Unable to moderate content")
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_103810) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_190157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_103810) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_accepted", default: false
+    t.string "content"
+    t.string "title"
   end
 
 end
